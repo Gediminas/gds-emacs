9140f1e2422443d9a0b8e06516ae24bed487358c
Gediminas <gediminas.luzys@gmail.com>
2020-02-27 13:56:50 +0200

    theme, which-key, general (https://dev.to/huytd/emacs-from-scratch-1cg6)

--- init.el
+++ init.el
@@ -40,6 +40,61 @@
   :config
   (evil-mode 1))
 
+;; Theme
+;(use-package doom-themes :ensure t)
+(use-package zenburn-theme
+  :ensure t
+  :config
+  (load-theme 'zenburn t))  
+
+;; Helm/ivy
+(use-package ivy
+  :ensure t
+  :init
+  )
+
+;; Which Key
+(use-package which-key
+  :ensure t
+  :init
+  (setq which-key-separator " ")
+  (setq which-key-prefix-prefix "+")
+  :config
+  (which-key-mode))
+
+;; Custom keybinding
+(use-package general
+  :ensure t
+  :config (general-define-key
+  :states '(normal visual insert emacs)
+  :prefix "SPC"
+  :non-normal-prefix "M-SPC"
+  ;; "/"   '(counsel-rg :which-key "ripgrep") ; You'll need counsel package for this
+  "TAB" '(switch-to-prev-buffer :which-key "previous buffer")
+  "SPC" '(helm-M-x :which-key "M-x")
+  "pf"  '(helm-find-file :which-key "find files")
+  ;; Buffers
+  "bb"  '(helm-buffers-list :which-key "buffers list")
+  ;; Window
+  "wl"  '(windmove-right :which-key "move right")
+  "wh"  '(windmove-left :which-key "move left")
+  "wk"  '(windmove-up :which-key "move up")
+  "wj"  '(windmove-down :which-key "move bottom")
+  "w/"  '(split-window-right :which-key "split right")
+  "w-"  '(split-window-below :which-key "split bottom")
+  "wx"  '(delete-window :which-key "delete window")
+  ;; Others
+  "at"  '(ansi-term :which-key "open terminal")
+))
+
+;; Fancy titlebar for MacOS
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+(add-to-list 'default-frame-alist '(ns-appearance . dark))
+(setq ns-use-proxy-icon  nil)
+(setq frame-title-format nil)
+
+
+
 ;(use-package avy :ensure t
 ;  :commands (avy-goto-word-1))
 ;
