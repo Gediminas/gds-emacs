33679b00d0a57bbc2af09060003956b733d311dc
Gediminas <gediminas.luzys@gmail.com>
2020-02-29 09:43:44 +0200

    use-package: init vs config

--- init.el
+++ init.el
@@ -62,10 +62,9 @@
 ;; Which Key
 (use-package which-key
    :ensure t
-   :init
+   :config
    (setq which-key-separator " ")
    (setq which-key-prefix-prefix "+")
-;   :config
    (which-key-mode t))
 
 ; ;; Helm/ivy
