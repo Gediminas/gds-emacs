75f5e96b2859de7b3919e195e25c8e2812d23ac3
Gediminas <gediminas.luzys@gmail.com>
2020-03-11 13:21:44 +0200

    Migration to config.org

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+;;BOOT-TIMER
 ;; Use a hook so the message doesn't get clobbered by other messages.
 (add-hook 'emacs-startup-hook
           (lambda ()
@@ -11,6 +12,7 @@
 ;; For macOS users:
 ;open -n /Applications/Emacs.app --args -q --eval='(message "%s" (emacs-init-time))'
 
+;;BOOT-FASTER
 ;; Make startup faster by reducing the frequency of garbage
 ;; collection.  The default is 800 kilobytes.  Measured in bytes.
 (setq gc-cons-threshold (* 50 1000 1000))
@@ -23,116 +25,22 @@
                          ("marmalade" . "http://marmalade-repo.org/packages/")))
 (package-initialize)
 
-;; Bootstrap `use-package'
+;;USE-PACKAGE
 (unless (package-installed-p 'use-package) ; unless it is already installed
   (package-refresh-contents) ; updage packages archive
   (package-install 'use-package)) ; and install the most recent version of use-package
 (require 'use-package) ; guess what this one does too ?
 
+;;THEME
 (use-package zenburn-theme :ensure t :config (load-theme 'zenburn t))
 (use-package doom-themes :ensure t)
 (use-package spacemacs-common :ensure spacemacs-theme)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
+;;CONFIG.ORG
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
-;; UI
-;(add-to-list 'default-frame-alist '(height . 40))
-;(add-to-list 'default-frame-alist '(width . 80))
-(scroll-bar-mode -1)
-(tool-bar-mode   -1)
-(tooltip-mode    -1)
-(menu-bar-mode   -1)
 
-;; MacOS: Fancy titlebar
-(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
-(add-to-list 'default-frame-alist '(ns-appearance . dark))
-(setq ns-use-proxy-icon  nil)
-(setq frame-title-format nil)
-
-;; MacOS
-(setq mac-command-modifier 'control)
-
-
-;; Advanced settings
-(desktop-save-mode t)     ;auto save/load session,window-size
-
-(setq-default truncate-lines t ;do not wrap lines
- )
-
-;; Minimize mode-line
-(use-package diminish
-  :ensure t
-  :config
-  (add-hook 'emacs-lisp-mode-hook 
-    (lambda()
-      (setq mode-name "")))  
-  (with-eval-after-load 'undo-tree
-    (diminish 'undo-tree-mode "")) 
-  (diminish 'eldoc-mode "")
-  )
-
-;; Vim mode
-(use-package evil
-  :ensure t
-  :config
-  (evil-mode t)
-)
-
-;; Which Key
-(use-package which-key
-  :ensure t
-  :init
-  (which-key-mode t)
-  :config
-  (which-key-setup-side-window-bottom)
-  (setq
-    which-key-sort-order 'which-key-key-order-alpha
-    which-key-side-window-max-width 0.33
-    which-key-idle-delay 0.05
-    which-key-separator " "
-    which-key-prefix-prefix "+")
-  :diminish (which-key-mode . ""))
-
-;; Ivy
-(use-package ivy
-  :ensure t
-  :config
-  (ivy-mode t)
-  (setq 
-    ivy-use-virtual-buffers t ; add ‘recentf-mode’ and bookmarks to ‘ivy-switch-buffer’
-    ivy-height 10             ; number of result lines to display
-    ivy-count-format "%d/%d " ; count candidates
-    ivy-initial-inputs-alist nil ; no regexp by default
-    ivy-re-builders-alist     ; configure regexp engine.
-        '((t   . ivy--regex-ignore-order));; allow input not in order
-   ) 
-  :diminish (ivy-mode . ""))
-
-(use-package counsel
-  :ensure t
-  :config
-  (counsel-mode t)
-  :diminish (counsel-mode . "")
-)
-
-;; Ranger (test)
-(use-package ranger
-  :ensure t
-  :commands (ranger)
-  :bind (("C-x d" . deer))
-  :config
-  (setq ranger-cleanup-eagerly t) ; kill the buffer just after you move to another entry in the dired buffer.
-  )
-
-;(use-package avy :ensure t
-;  :commands (avy-goto-word-1))
-
-;(global-set-key (kbd "<C-tab>") 'next-buffer)
-
-
+;;CUSTOM.EL
 ;; Keep 'Customize' stuff separated
 (setq custom-file (concat user-emacs-directory "custom.el"))
 (load custom-file 'noerror)
