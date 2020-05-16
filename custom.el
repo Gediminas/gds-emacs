be536e56050a5bb15e0bef0ab927c882915be86f
Gediminas <gediminas.luzys@gmail.com>
2020-05-16 09:31:47 +0300

    highlights, scrolling

--- custom.el
+++ custom.el
@@ -66,4 +66,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "outline" :family "Hack"))))
- '(region ((t (:background "blue4")))))
+ )
