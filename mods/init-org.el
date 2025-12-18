d6ab0ab9b802fa7d38de3d4de6c2699518d8c881
Gediminas Lūžys <163294+gediminas@users.noreply.github.com>
2025-12-18 18:03:49 +0200

    Refactor org

--- mods/init-org.el
+++ mods/init-org.el
@@ -1,23 +1,28 @@
-;;; -*- lexical-binding: t; -*-
+;;; init-org.el --- Org-mode configuration -*- lexical-binding: t; -*-
 
-(setq org-directory "~/org")                 ; Main folder for notes
-(setq org-agenda-files (list org-directory)) ; Where agenda looks for files
-(setq org-default-notes-file (concat org-directory "/inbox.org")) ; default capture file
-(setq org-startup-indented t)                ; Pretty indentation
-(setq org-hide-leading-stars t)              ; Cleaner headings
-(setq org-return-follows-link t)             ; Enter key opens links
-(setq org-startup-folded 'content)           ; Start folded, outline view
-(setq org-log-done 'time)                    ; Log DONE
+(use-package org
+  :mode ("\\.org\\'" . org-mode)
+  
+  :custom
+  (org-directory "~/org")
+  (org-default-notes-file (expand-file-name "inbox.org" org-directory))
+  (org-agenda-files (list org-directory))
+  (org-hide-leading-stars t)
+  (org-return-follows-link t); Enter key opens links
+  (org-startup-indented t); Pretty indentation
+  (org-startup-folded 'content); Start folded, outline view
+  (org-log-done 'time)
 
-(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode)) ; *.org -> org mode
-(add-hook 'org-mode-hook 'org-indent-mode)   ; Make the indentation look nicer
-
-(setq org-capture-templates
-  '(("j" "Work-Log" entry (file+datetree "~/org/work-log.org")  "* %?"  :empty-lines 0)
-    ("n" "Note"     entry (file+headline "~/org/notes.org" "Random Notes") "** %?" :empty-lines 0)))
-
-(global-set-key (kbd "C-c a") #'org-agenda)     ; Open agenda
-(global-set-key (kbd "C-c c") #'org-capture)    ; Quick capture
-(global-set-key (kbd "C-c l") #'org-store-link) ; Store links
+  (org-capture-templates
+   '(("j" "Work-Log" entry (file+datetree "~/org/work-log.org")              "* %?" :empty-lines 0)
+     ("n" "Note"     entry (file+headline "~/org/notes.org" "Random Notes") "** %?" :empty-lines 0)))
+  
+  :hook
+  (org-mode . org-indent-mode)  ; Make the indentation look nicer
+  
+  :bind
+  (("C-c a" . org-agenda)
+   ("C-c c" . org-capture)
+   ("C-c l" . org-store-link)))
 
 (provide 'init-org)
